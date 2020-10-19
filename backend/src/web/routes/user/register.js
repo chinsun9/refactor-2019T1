f740172f1d9cd81aebea41c7facf9995d4ce686d
chinsun9 <40482203+chinsun9@users.noreply.github.com>
2020-10-19 10:38:16 +0900

    fix: Duplicate ID

--- backend/src/web/routes/user/register.js
+++ backend/src/web/routes/user/register.js
@@ -10,53 +10,60 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-  var userId = req.body.userId;
-  var userPw = req.body.userPw;
-  var userName = req.body.userName;
-  var userEmail = req.body.userEmail;
-  var userEmailStatus = req.body.userEmailStatus;
-  var userBirth = req.body.userBirth;
-  var userSex = req.body.userSex;
-  var userArea = req.body.userArea;
+  const {
+    userId,
+    userPw,
+    userName,
+    userEmail,
+    userEmailStatus,
+    userBirth,
+    userSex,
+    userArea,
+  } = req.body;
 
-  var count = 0;
+  let userNum;
 
-  console.log(req.body);
+  // id 중복검사
 
-  var results1 = client.query('select count(*) as count from luciddb.USER');
+  const chkID = client.query('SELECT * FROM luciddb.USER where userId=?', [
+    userId,
+  ]);
 
-  results1.forEach((item, index) => {
-    count = item.count;
-  });
+  if (chkID.length !== 0) {
+    throw new Error('Duplicate ID');
+  }
 
-  console.log(count + '');
-
-  var results2 = client.query(
-    'insert into luciddb.USER values (' +
-      (count + 1) +
-      ', "' +
-      userId +
-      '", "' +
-      userPw +
-      '", "' +
-      userName +
-      '", "' +
-      userEmail +
-      '", ' +
-      userEmailStatus +
-      ', "' +
-      userBirth +
-      '", ' +
-      userSex +
-      ', ' +
-      userArea +
-      ')'
+  const results1 = client.query(
+    'SELECT MAX(userNum) as lastNum FROM luciddb.USER'
   );
-  var results3 = client.query(
-    'insert into luciddb.SERVICEEOGORDER values (' +
-      (count + 1) +
-      ', 0, 1, 2, 3, 4, 5, 6, 7, 8)'
+
+  if (results1.length === 0) {
+    userNum = 1;
+  } else {
+    userNum = results1[0].lastNum + 1;
+  }
+
+  console.log(userNum + '');
+
+  client.query('insert into luciddb.USER values (?, ?, ?, ?, ?, ?, ?, ?, ?)', [
+    userNum,
+    userId,
+    userPw,
+    userName,
+    userEmail,
+    userEmailStatus,
+    userBirth,
+    userSex,
+    userArea,
+  ]);
+
+  client.query(
+    'insert into luciddb.SERVICEEOGORDER values (?, 0, 1, 2, 3, 4, 5, 6, 7, 8)',
+    [userNum]
   );
+
+  req.session.loginMessage = 'Sign-up successful! Please Sign-in.';
+
   res.redirect('/web/login');
 });
 
