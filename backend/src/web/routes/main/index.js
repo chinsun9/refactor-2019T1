1428826714f2093670bc9cd5059ec971206efcb7
chinsun9 <40482203+chinsun9@users.noreply.github.com>
2020-10-19 10:58:31 +0900

    refactor: var to (let | const)

--- backend/src/web/routes/main/index.js
+++ backend/src/web/routes/main/index.js
@@ -243,29 +243,4 @@ router.get('/', (req, res) => {
   });
 });
 
-router.post('/', (req, res) => {
-  var userId = req.body.userId;
-  var userPw = req.body.userPw;
-
-  var user = '';
-
-  var results = client.query(
-    'select userNum from luciddb.USER where userId = "' +
-      userId +
-      '" and userPw = "' +
-      userPw +
-      '"'
-  );
-
-  results.forEach((item, index) => {
-    user = item.userNum;
-  });
-
-  if (user != '') {
-    res.redirect('/web/main/index');
-  } else {
-    res.redirect('/web/login');
-  }
-});
-
 module.exports = router;
--- backend/src/web/routes/user/logout.js
+++ backend/src/web/routes/user/logout.js
@@ -2,10 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  // 로그아웃, 쿠키 날리기
   req.session.userNum = '';
 
-  // 로그인 페이지로
   res.redirect('/web/login');
 });
 
